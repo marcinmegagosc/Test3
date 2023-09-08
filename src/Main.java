be616d49bb0e11b6eac1319aa2322599c994bb69
marcinmegagosc <marcin.sss221@gmail.com>
2023-09-08 09:18:55 +0200

    bez komentarzy

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+     int x = 1;
+     int number;
+        System.out.println("napisz liczbe");
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
+     for(int i=1; i<=number;i++){
+         x=x*i;
+     }
+        System.out.println("Silnia z "+number+ " wynosi " +x);
     }
 }
